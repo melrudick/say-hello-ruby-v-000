0da265614d994cdefd8a11679b99b4f7957bf160
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-25 04:55:30 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,6 +3,6 @@ def say_hello(name)
   puts "Hello, Kent Beck!"
 end
 
-def say_hello(name="Ruby Programmer!")
-  puts "Hello #{name}"
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
 end
